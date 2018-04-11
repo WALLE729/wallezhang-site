2dc5295832a10f33c24650c4ba59970f6704e233
walle瓦力 <walle89729@gmail.com>
2018-04-12 00:17:57 +0800

    update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,38 +1,37 @@
-import React, {Component} from 'react'
-import { Menu, Icon, Row, Col } from 'antd';
+import React, { Component } from 'react'
+import { Menu, Icon, Row, Col } from 'antd'
 import { Link } from 'react-router-dom'
 import Logo from './../assets/img/robot.svg'
 import './css/Header.css'
-const SubMenu = Menu.SubMenu;
-const MenuItemGroup = Menu.ItemGroup;
+const SubMenu = Menu.SubMenu
+const MenuItemGroup = Menu.ItemGroup
 
-class Header extends Component{
-    render() {
-        return (
-            <div className="header_box">
-             <Row>
-                <Col span={12}>
-                    <span className="logo_box">
-                        <Link to="/">
-                            <img width='40' src={ Logo } alt="logo" />
-                            <span className="logo_text">WALLE</span>
-                        </Link> 
-                    </span>
-                </Col>
-                <Col span={12}>
-                    <Menu mode="horizontal">
-                        <Menu.Item key="mail">
-                        <Link to="/"> Home </Link>
-                        </Menu.Item>
-                        <Menu.Item key="app">
-                        <Link to="/about"> about </Link>
-                        </Menu.Item>
-                    </Menu>
-                </Col>
-            </Row>
-                
-            </div>
-        )
-    }
+class Header extends Component {
+  render() {
+    return (
+      <div className="header_box">
+        <Row>
+          <Col span={12}>
+            <span className="logo_box">
+              <Link to="/">
+                <img width="40" src={Logo} alt="logo" />
+                <span className="logo_text">WALLE</span>
+              </Link>
+            </span>
+          </Col>
+          <Col span={12}>
+            <Menu mode="horizontal">
+              <Menu.Item key="mail">
+                <Link to="/"> Home </Link>
+              </Menu.Item>
+              <Menu.Item key="app">
+                <Link to="/about"> about </Link>
+              </Menu.Item>
+            </Menu>
+          </Col>
+        </Row>
+      </div>
+    )
+  }
 }
 export default Header
--- src/containers/HomeContainer/component/HomeCarousel.js
+++ src/containers/HomeContainer/component/HomeCarousel.js
@@ -6,22 +6,28 @@ import walle2 from './../../../assets/img/walle2.jpg'
 import walle3 from './../../../assets/img/walle3.jpg'
 import walle4 from './../../../assets/img/walle4.jpg'
 function onChange(a, b, c) {
-    console.log(a, b, c);
+  console.log(a, b, c)
 }
-class HomeCarousel extends Component{
-    render() {
-        return (
-            <div className="home_banner_box">
-                <Carousel afterChange={onChange} autoplay>
-                    <div>
-                        <img width="100%" height="auto" src={walle1} alt="banner" />
-                    </div>
-                    <div><img width="100%" height="auto" src={walle2} alt="banner" /></div>
-                    <div><img width="100%" height="auto" src={walle3} alt="banner" /></div>
-                    <div><img width="100%" height="auto" src={walle4} alt="banner" /></div>
-                </Carousel>
-            </div>
-        )
-    }
+class HomeCarousel extends Component {
+  render() {
+    return (
+      <div className="home_banner_box">
+        <Carousel afterChange={onChange} autoplay>
+          <div>
+            <img width="100%" height="auto" src={walle1} alt="banner" />
+          </div>
+          <div>
+            <img width="100%" height="auto" src={walle2} alt="banner" />
+          </div>
+          <div>
+            <img width="100%" height="auto" src={walle3} alt="banner" />
+          </div>
+          <div>
+            <img width="100%" height="auto" src={walle4} alt="banner" />
+          </div>
+        </Carousel>
+      </div>
+    )
+  }
 }
 export default HomeCarousel
